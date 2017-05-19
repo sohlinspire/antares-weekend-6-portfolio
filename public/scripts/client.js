f8f067f60d8b3c963088a41099a2e19a5a7fced0
Huck Brock <huckbrock@gmail.com>
2017-05-19 15:17:03 -0500

    removed code from controller

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,14 +3,6 @@ console.log('test');
 var myApp = angular.module('myApp', []);
 
 
-myApp.controller('NameController', function($http) {
-  $http({
-    method: 'GET',
-    url: 'https://api.github.com/users/mhwalsh/repos',
-    headers: {
-    'Authorization': 'token 25636dcb5d81019574277cd51f0fda58d3297f94'}
-    }
-  ).then(function(response) {
-    console.log(response);
-  });
+myApp.controller('NameController', function() {
+
 });
