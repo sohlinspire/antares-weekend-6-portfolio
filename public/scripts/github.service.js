53188b23ae5b33bed67073b4e19d9bb293d0afcd
Huck Brock <huckbrock@gmail.com>
2017-05-19 15:14:06 -0500

    adds service

---
+++ public/scripts/github.service.js
@@ -0,0 +1,37 @@
+myApp.controller('GithubAPI', function($http) {
+  //Enter your token and username here:
+  var oauthToken = '';
+  var username = '';
+
+  //Call to Github API to fetch user's profile info
+  this.githubProfile = function(){
+
+    return $http({
+      method: 'GET',
+      url: 'https://api.github.com/users/' + username,
+      headers: {
+        'Authorization': 'token '+ oauthToken}
+      }
+    ).then(function(response) {
+      console.log(response.data);
+      return response.data;
+    });
+  }
+
+  //Call to Github API to fetch list of user's repos
+  this.githubRepos = function(){
+
+    return $http({
+      method: 'GET',
+      url: 'https://api.github.com/users/' + username + '/repos',
+      headers: {
+        'Authorization': 'token '+ oauthToken}
+      }
+    ).then(function(response) {
+      console.log(response.data);
+      return response.data;
+    });
+  }
+
+
+});
